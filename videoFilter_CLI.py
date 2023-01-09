93e9d8aff4c2c7c7c4ef625de282c2e41636bf88
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-01-09 15:44:05 +0100

    Update videoFilter_CLI.py

--- videoFilter_CLI.py
+++ videoFilter_CLI.py
@@ -6,3 +6,12 @@ import numpy as np
 import threading
 import os
 import argparse
+
+def main():
+
+    parser = argparse.ArgumentParser(prog="videoFilter_CLI",description="Video filter on CLI")
+    parser.add_argument('-src','--source',required=True,type=str,help='Source video')
+    parser.add_argument('-dest','--destination',type=str,help='Destination video')
+    parser.add_argument('-flt','--filter',type=str,default='bilateral',choices=['bilateral','blur','median','denoisingCol','2d','pyrdown','sketched','mean'],help='Filter method')
+    
+
