f567dbabf3b49a42e49c2f450a1e79a00777b186
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-03-14 17:05:40 +0100

    Update vf_CLI.py

--- vf_CLI.py
+++ vf_CLI.py
@@ -10,6 +10,9 @@ from pydub import AudioSegment
 from tqdm import tqdm
 import argparse
 
+# vf_CLI.py -src doaxx.mp4 -dest doaxx_2dfilt.mp4 -flt 2d -ea +
+# vfb_CLI.py -src doaxx_2dfilt.mp4 dest doaxx_filt13.mp3 -ea -d -pd 13
+
 n_frames = 0
 frame_list = []
 frame_rate = ""
@@ -147,26 +150,31 @@ def frames_editor(filterm,source):
     
 def app(args):
     global n_frames, frame_rate
-    if args.source in os.listdir():
-        probe = ffmpeg.probe(args.source)
-        video_streams = [stream for stream in probe["streams"] if stream["codec_type"] == "video"]
-        n_frames = (video_streams[0]['nb_frames'])
-        height = (video_streams[0]['height'])
-        width = (video_streams[0]['width'])
-        frame_rate = (video_streams[0]['avg_frame_rate'])
-        codec_type = (video_streams[0]['avg_frame_rate'])
-        print(Fore.YELLOW+"\n******************VIDEO INFO******************")
-        print(f'SOURCE FILE: {args.source}')
-        print(f'Number of frames: {n_frames}')
-        print(f'Frame Rate: {frame_rate}')
-        print(f'Width: {width}')
-        print(f'Height: {height}')
-        print("**********************************************\n"+Fore.RESET)
-        frames_editor(args.filter,args.source)
-        if check == True:
-            create_video(args)
+    name, ex = os.path.splitext(args.source)
+    name2, ex2 = os.path.splitext(args.destination)
+    if ex == ".mp4" and ex2 == ".mp4":
+        if args.source in os.listdir():
+            probe = ffmpeg.probe(args.source)
+            video_streams = [stream for stream in probe["streams"] if stream["codec_type"] == "video"]
+            n_frames = (video_streams[0]['nb_frames'])
+            height = (video_streams[0]['height'])
+            width = (video_streams[0]['width'])
+            frame_rate = (video_streams[0]['avg_frame_rate'])
+            codec_type = (video_streams[0]['avg_frame_rate'])
+            print(Fore.YELLOW+"\n******************VIDEO INFO******************")
+            print(f'SOURCE FILE: {args.source}')
+            print(f'Number of frames: {n_frames}')
+            print(f'Frame Rate: {frame_rate}')
+            print(f'Width: {width}')
+            print(f'Height: {height}')
+            print("**********************************************\n"+Fore.RESET)
+            frames_editor(args.filter,args.source)
+            if check == True:
+                create_video(args)
+        else:
+            print(Fore.RED+Style.DIM+f"\nERROR: File '{args.source}' not found."+Fore.RESET+Style.RESET_ALL)
     else:
-        print(Fore.RED+Style.DIM+f"\nERROR: File '{args.source}' not found."+Fore.RESET+Style.RESET_ALL)
+        print(Fore.RED+Style.DIM+"\nBAD FILE FORMAT: Source file and destination file must have 'mp4' extension."+Fore.RESET+Style.RESET_ALL)
 
 if __name__=="__main__":
     main()
